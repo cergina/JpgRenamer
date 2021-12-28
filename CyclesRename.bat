83b485e0ca246885dc64fe0c2696d45150a3076e
Maros Cerget <mcerget@gmail.com>
2021-12-28 19:00:52 +0100

    Added support for turbosquid

--- CyclesRename.bat
+++ CyclesRename.bat
@@ -1,40 +1,87 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+echo:
+echo:
 echo ============================================================================================================
-echo  Program to rename JPG files 0001.jpg, 0005.jpg to [param_name]_[0+1*step].jpg, [param_name]_[0+2*step].jpg, ...
+echo  Program to rename Blender output jpg files 0001.jpg, 0005.jpg to desired naming convention
 echo ============================================================================================================
 echo 	Start in Dir where images are
-echo    call this in way: CyclesRename.bat [param_name] [step] in the directory
+echo    CALLING USAGE: 'CyclesRename.bat [param_name] [step] [turbosquid]' in the directory
+echo:
+echo   [param_name]		=  anything like eg.: 'image'
+echo   [step]		=  use 1 if you want output eg.: 'image_1', 'image_2', ...
+echo   [turbosquid]		=  use 'true' if you want eg.: 'image_01', 'image_02', ... for turbosquid format, MAX 72 pictures tho
+echo:
 
-echo Parameter name: %1%
-echo Step is: %2%
+echo -----------
+echo  Program started, parameters were
+echo:
+echo $parameter_name: %1%
+echo $step: %2%
+echo $turbosquid: %3%
 
+echo:
+echo -----------
+echo: 
+
+REM helper variables
 set /A NewStep= 0
 set /A CurStep= 0
-set /A Inc= 1
+set Padding=%3%
 
-mkdir OUTPUT
+REM minimum step is 1, this requires delayedexpansion  enabled
+if %2% LSS 1 (
+	set /A Inc=1
+) else (
+	set /A Inc=%2%
+)
 
-echo ============================================================================================================
+REM if used padding for turbosquid it will have leading zero
+if "%Padding%"=="true" (
+	echo HAHA
+	set Padding=0
+) else (
+	echo HEHE
+	set Padding=
+)
+
+REM uncomment in case you want to stop code here
+REM goto :eof 
+
+mkdir output
+
+echo -----------
 for %%x in (*.jpg) do call :body %%x %1%
 goto :afterbody
 
 :body
 echo Old File Name: "%1"
 	echo ..
+	REM numeric assignment
 	set /A NewStep= %CurStep% + %Inc%
 	echo Current: [%CurStep%] , Next: [%NewStep%]
 	echo ..
+	
+	REM for next round
+	set /A CurStep = %NewStep%
+	
+	REM give leading zero in case of turbosquid, appliable when lower than 10
+	if %NewStep% LSS 10 (
+		set NewStep=%Padding%%NewStep%
+	)
+
 	set NewFName=%2_%NewStep%.jpg
 	echo New File Name: %NewFName%
 	::::ren %1 %NewFName%
-	echo Copying %cd%\%1 >> into >> %cd%\OUTPUT\%NewFName%
-	copy %cd%\%1 %cd%\OUTPUT\%NewFName%
-	set /A CurStep = %NewStep%
+	echo Copying %cd%\%1 >> into >> %cd%\output\%NewFName%
+	copy %cd%\%1 %cd%\output\%NewFName%
+	
 	echo =========
 goto :eof
 
 :afterbody
 echo ============================================================================================================
+echo: 
 pause
+echo:
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # JpgRenamer
+## REASONS OF CREATION
+Turbosquid is a platform where you can sell 3D models. They require turntable model previews but you need to use specific naming convention like "hello_00.jpeg; hello_01.jpeg;" with leading zero necessary.
+You are also allowed to have only 72 images at max, forcing me to use step 5 during 360 render which results in output like "0001.jpg; 0005.jpg;"
+
+Summed up. Didn't want to do renaming by hand every time I needed to fullfil  these:
+
+- uploading pictures somewhere where naming format required
+- leading zeroes required "name_01"
+- unwanted steps in original naming "0001" -> "0005"
+
 ## DESCRIPTION
 BAT file to take all jpg files in current directory, rename them into desired format and put into output directory
 
@@ -11,21 +21,33 @@ Renaming them all BY HAND would be really time consuming.
 0001.jpg
 0004.jpg
 0008.jpg
+...
+
+### Case 0: Turbosquid turntable requires 12 - 72 jpegs
+model_01.jpg
+model_02.jpg
+model_03.jpg
+...
 
 ### Case 1 : You want to rename them to lets say
 image_1.jpg
 image_2.jpg
 image_3.jpg
+...
 
 ### Case 2 : You want to have a step difference (eg.: 4) between them
 
 image_4.jpg
 image_8.jpg
 image_12.jpg
+...
 
 ## Calling
 
 CyclesRename.bat [param_name] [step]
+
+### case 0 
+CyclesRename.bat model 1 true
 
 ### case 1 
 CyclesRename.bat image 1
